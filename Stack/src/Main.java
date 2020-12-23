e1a26d9c04b9ec7a1acf74d100e83cc4495ce16b
Jeremy Persing <jpersing1999@gmail.com>
2020-12-23 12:12:38 -0800

    Stack data structure

---
+++ Stack/src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+
+    static class Node {
+        Node next;
+        int value;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    static class Stack {
+        Node top;
+
+        void push(int value) {
+            if (top == null) {
+                top = new Node(value);
+            }
+            else {
+                Node newNode = new Node(value);
+                newNode.next = top;
+                top = newNode;
+            }
+        }
+
+        int pop() {
+            Node returnNode = top;
+            top = top.next;
+            return returnNode.value;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+	    for (int i = 0; i < 5; i++) {
+	        stack.push(i);
+        }
+	    System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println("Top of stack value: " + stack.top.value);
+    }
+}
